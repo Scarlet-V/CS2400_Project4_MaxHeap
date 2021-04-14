86061049ab2ab773371356bc2ac0647903b9660e
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-04-13 22:56:43 -0700

    added public boolean isEmpty();

--- MaxHeapProject/MaxHeapInterface.java
+++ MaxHeapProject/MaxHeapInterface.java
@@ -5,4 +5,5 @@ public interface MaxHeapInterface<T extends Comparable<? super T>>
     public void add(T newEntry);
     public T removeMax();
     public T getMax();
+    public boolean isEmpty();
 }

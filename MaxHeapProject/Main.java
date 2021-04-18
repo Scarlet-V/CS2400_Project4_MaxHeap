0d675b1a1dbf89d41485ec07c3079f27dfdf7f5c
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-04-17 22:27:55 -0700

    adding txt files and adding while loop

--- MaxHeapProject/Main.java
+++ MaxHeapProject/Main.java
@@ -7,8 +7,23 @@ public class Main
 {
     public static void main(String arg[])
     {
-    
-    
+        try{
+            MaxHeapInterface heap = new MaxHeap<Integer>(100);
+            
+            File file = new File("datasorted.txt");
+            File outFile=new File("output.txt");
+            RandomAccessFile inpRaf= new RandomAccessFile(file,"rw");
+            RandomAccessFile raf= new RandomAccessFile(outFile,"rw");
+            raf.writeBytes("Heap built using sequential insertion: ");
+            Integer num;
+            Integer array[]=new Integer[100];
+            int count=0;
+            while(inpRaf.getFilePointer()<inpRaf.length())
+        {
+            num=Integer.parseInt(inpRaf.readLine());
+            array[count++]=num;
+            heap.add(num);
+        }
     
     
     }    

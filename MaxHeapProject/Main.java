d7c4284afc28a848b7a05d781f86a6c10c97846a
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-04-17 23:32:20 -0700

    still trying to figure out compilation errors... fixed some

--- MaxHeapProject/Main.java
+++ MaxHeapProject/Main.java
@@ -2,28 +2,27 @@ package MaxHeapProject;
 
 import java.util.*;
 import java.io.*;
-
-public class Main 
-{
+public class Main {
     public static void main(String arg[])
     {
         try{
-            MaxHeapInterface heap = new MaxHeap<Integer>(100);
-            
-            File file = new File("datasorted.txt");
-            File outFile=new File("output.txt");
-            RandomAccessFile inpRaf= new RandomAccessFile(file,"rw");
-            RandomAccessFile raf= new RandomAccessFile(outFile,"rw");
-            raf.writeBytes("Heap built using sequential insertion: ");
-            Integer num;
-            Integer array[]=new Integer[100];
-            int count=0;
-            while(inpRaf.getFilePointer()<inpRaf.length())
+        MaxHeapInterface heap = new MaxHeap<Integer>(100);
+        
+        File file = new File("datasorted.txt");
+        File outFile=new File("output.txt");
+        RandomAccessFile inpRaf= new RandomAccessFile(file,"rw");
+        RandomAccessFile raf= new RandomAccessFile(outFile,"rw");
+        raf.writeBytes("Heap built using sequential insertion: ");
+        Integer num;
+        Integer array[]=new Integer[100];
+        int count=0;
+        while(inpRaf.getFilePointer()<inpRaf.length())
         {
             num=Integer.parseInt(inpRaf.readLine());
             array[count++]=num;
             heap.add(num);
         }
+        
         for(int i=1;i<=10;++i)
         {
             raf.writeBytes(String.valueOf((Integer)heap.getElementAt(i))+" ");
@@ -31,29 +30,20 @@ public class Main
         raf.writeBytes("\n");
         raf.writeBytes("Number Of Swaps performed in Sequential Heap: "+heap.getSeqSwap()+"\n");
         raf.writeBytes("Heap after 10 removals: ");
-        for(int i=0;i<10;++i) 
-        {
-            heap.removeMax();
-        }
-        for(int i=1;i<=10;++i) 
-        {
-            raf.writeBytes(String.valueOf((Integer)heap.getElementAt(i))+" ");
-            raf.writeBytes("\n");
-            MaxHeapInterface optHeap=new MaxHeap(array);
-            raf.writeBytes("Heap built using optimal insertion: ");
-        }
-
-        or(int i=1;i<=10;++i)
+        
+        for(int i=0;i<10;++i) heap.removeMax();
+        for(int i=1;i<=10;++i) raf.writeBytes(String.valueOf((Integer)heap.getElementAt(i))+" ");
+        raf.writeBytes("\n");
+        MaxHeapInterface optHeap=new MaxHeap(array);
+        raf.writeBytes("Heap built using optimal insertion: ");
+        for(int i=1;i<=10;++i)
         {
             raf.writeBytes(String.valueOf(optHeap.getElementAt(i))+" ");
         }
         raf.writeBytes("\n");
         raf.writeBytes("Number Of Swaps performed in Sequential Heap: "+optHeap.getOptSwap()+"\n");
         raf.writeBytes("Heap after 10 removals: ");
-        for(int i=0;i<10;++i) 
-        {
-            optHeap.removeMax();
-        }
+        for(int i=0;i<10;++i) optHeap.removeMax();
         for(int i=1;i<=10;++i)
         {
             raf.writeBytes(String.valueOf(optHeap.getElementAt(i))+" ");
@@ -66,8 +56,6 @@ public class Main
     {
         System.out.println(io);
     }
-
-    
-    
-    }    
+    }
+   
 }
